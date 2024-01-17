35f4581361fba3b2430c79869c6dc8813d1c9ae4
Agustin Ortiz <aortizc01@gmail.com>
2024-01-17 13:29:05 +0100

    first commit

---
+++ dist/memoize.js
@@ -0,0 +1,31 @@
+"use strict";
+// // Definir una función memoize genérica
+// function memoize<T, U>(func: (arg: T) => U): (arg: T) => U {
+//   const cache: Map<T, U> = new Map();
+//   return (arg: T) => {
+//     if (cache.has(arg)) {
+//       // Si el resultado está en la caché, devolverlo directamente
+//       return cache.get(arg)!;
+//     }
+//     // Si no está en la caché, calcular el resultado y almacenarlo en la caché
+//     const result = func(arg);
+//     cache.set(arg, result);
+//     return result;
+//   };
+// }
+// // Ejemplo de una función costosa de cálculo
+// function calcularFactorial(n: number): number {
+//   if (n === 0 || n === 1) {
+//     return 1;
+//   } else {
+//     return n * calcularFactorial(n - 1);
+//   }
+// }
+// // Aplicar memoize a la función de cálculo del factorial
+// const memoizedFactorial = memoize(calcularFactorial);
+// // Ejemplos de uso
+// console.log(memoizedFactorial(5)); // Calcula y almacena el factorial de 5
+// console.log(memoizedFactorial(5)); // Obtiene el resultado almacenado de la caché
+// console.log(memoizedFactorial(3)); // Calcula y almacena el factorial de 3
+// console.log(memoizedFactorial(3)); // Obtiene el resultado almacenado de la caché
+//# sourceMappingURL=memoize.js.map
---
+++ src/__tests__/memoize.test.ts
@@ -0,0 +1,41 @@
+// Definir una función memoize para la suma de dos números
+function memoizeSum(a: number, b: number): number {
+  const cache: Map<string, number> = new Map();
+
+  const key = `${a}-${b}`;
+
+  if (cache.has(key)) {
+    // Si el resultado está en la caché, devolverlo directamente
+    return cache.get(key)!;
+  }
+
+  // Si no está en la caché, calcular el resultado y almacenarlo en la caché
+  const result = a + b;
+  cache.set(key, result);
+
+  return result;
+}
+
+// Ejemplos de uso de la función memoizeSum
+console.log(memoizeSum(2, 3)); // Calcula y almacena la suma de 2 y 3
+console.log(memoizeSum(2, 3)); // Obtiene el resultado almacenado de la caché
+
+console.log(memoizeSum(1, 4)); // Calcula y almacena la suma de 1 y 4
+console.log(memoizeSum(1, 4)); // Obtiene el resultado almacenado de la caché
+
+//import { describe } from "node:test";
+describe("memoizeSum", () => {
+  2;
+  3;
+});
+it("deberia poder sumar", () => {
+  expect(memoizeSum(2, 3)).toBe(5);
+});
+
+describe("memoizeSum", () => {
+  1;
+  4;
+});
+it("deberia poder sumar", () => {
+  expect(memoizeSum(1, 4)).toBe(5);
+});
